c3b6e95a1287cc11278303dc7426e5c7c05cd6d5
ari salli <swf2817@gmail.com>
2022-06-21 16:15:22 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,127 +1,21 @@
-import { useState, useEffect } from "react";
-import Question from "./components/Question";
+import QuizPage from "./components/QuizPage";
+import StartPage from "./components/StartPage";
+import { Route, Routes } from "react-router-dom";
 
 
 function App() {
-  const [isActive, setIsActive] = useState(false);
-  const [newQuiz, setNewQuiz] = useState(false);
-  const [quiz, setQuiz] = useState([]);
-  const [chosenOption, setChosenOption] = useState([]);
-  const [result, setResult] = useState();
-  const [checked, setChecked] = useState(false);
-
-  useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5&encode=url3986")
-      .then((res) => res.json())
-      .then((data) => setQuiz(getQuestions(data.results)));
-      const getQuestions = (data) => {
-        const questionsArray = [];
-        const answersArray = [];
-    
-        data.map((el) => {
-          answersArray.push({
-            correct: decodeURIComponent(el.correct_answer),
-            chosen: "",
-          });
-          const options = el.incorrect_answers;
-          options.push(el.correct_answer);
-          shuffle(options);
-          return questionsArray.push({
-            correct: el.correct_answer,
-            incorrect: options,
-            question: decodeURIComponent(el.question),
-          });
-        });
-        // setQuiz(questionsArray)
-        setChosenOption(answersArray);
-        return questionsArray;
-      };
-    
-      const shuffle = (array) => {
-        array.sort(() => Math.random() - 0.5);
-      };
-
-  }, [newQuiz]);
-
-  
-
-  const handleOnHold = (chosen, questionIndex) => {
-    setChosenOption((prevOption) => {
-      return prevOption.map((item, index) =>
-        index === questionIndex ? { ...item, chosen: chosen } : item
-      );
-    });
-  };
-
-  const getQuiz = quiz.map((el, index) => (
-    <Question
-      key={el.question}
-      correctAnswer={el.correct}
-      question={el.question}
-      answers={el.incorrect}
-      questionNumber={index}
-      handleClick={handleOnHold}
-      over={checked }
-      isNewGame={chosenOption.length === 0 && !checked}
-    />
-  ));
-
-  // console.log(getQuiz);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    let countCorrect = 0;
-    chosenOption.map((item) => item.correct === item.chosen && countCorrect++);
-    setResult(countCorrect);
-    setChecked(true);
-
-  };
-
-  const showQuiz = () => {
-    setIsActive(true);
-  };
-
-  const restartQuiz = () => {
-    setNewQuiz((prevState) => !prevState);
-    setChecked(false);
-    setResult("");
-    setChosenOption([]);
-
-  };
 
   return (
     <div className="App">
-      {isActive ? (
-        <div className="quizPage">
-          <div className="quiz-form">
-            {getQuiz}
-            <div className="quiz-result">
-              {checked ? (
-                <>
-                  <h3>You scored {result}/5 correct answers</h3>
-                  <button onClick={restartQuiz} className="button quiz-button">
-                    Restart
-                  </button>
-                </>
-              ) : (
-                <button onClick={handleSubmit} className="button quiz-button">
-                  Check answers
-                </button>
-              )}
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="startPage">
-          <h1>Quizzical</h1>
-          <p>
-            You will be given ten questions on completely different topics.
-            Let's see how well you can do.
-          </p>
-          <button className="button start-button" onClick={showQuiz}>
-            Start quiz
-          </button>
-        </div>
-      )}
+    <Routes>
+    <Route path="/" element={<StartPage />} />
+
+      <Route path="/start" element={<StartPage />}>
+        
+      </Route>
+      <Route path="/quiz" element={<QuizPage />}>
+      </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,74 +1,59 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 const Question = (props) => {
-  const options = props.answers
-  const [currentAnswer, setCurrentAnswer] = useState('')
+  const options = props.answers;
+  const correctOption = decodeURIComponent(props.correctAnswer);
+  const [currentAnswer, setCurrentAnswer] = useState("");
 
-
-  const decideStyle = (answer) => {
-    let buttonStyles = {backgroundColor: "#F5F7FB"} 
-    //style incorrect answer
-    if (answer===currentAnswer && currentAnswer!==decodeURIComponent(props.correctAnswer)) {
-      buttonStyles = {
-        backgroundColor: "#F8BCBC",
-        borderColor: "#F8BCBC",
-      }
-    } 
-    //style correct answer
-    if (answer===decodeURIComponent(props.correctAnswer)) {
-      buttonStyles = {
-        backgroundColor: "#94D7A2",
-        borderColor: "#94D7A2",
-        opacity: 1
-      }
-    }  
-    return buttonStyles
-  }
-
-  const chosenStyle = (answer) => {
-    let style = {backgroundColor: "#F5F7FB"}
-    if (answer===currentAnswer) {
-      style = {
-        backgroundColor: "#D6DBF5",
-        borderColor: "#D6DBF5"
-      }
-    }
-    return style
-  }
-
-  const usualStyle = {backgroundColor: "#F5F7FB"};
-  
   const getStyles = (option) => {
-    let style = usualStyle;
-    if (props.isNewGame) {
-      return decideStyle(option);
-    }
-    style = props.over ? decideStyle(option) : chosenStyle(option);
-    return style;
-  }
+    let style = "usual";
 
-  const getOptions = options.map(el => {
-    const option = decodeURIComponent(el)
+    if (!props.isNewGame && !props.over && option !== currentAnswer) {
+      return style;
+    }
+
+    if (!props.over && option === currentAnswer) {
+      return (style = "chosen");
+    }
+
+    if (props.isNewGame || props.over) {
+      if (option === currentAnswer && currentAnswer !== correctOption) {
+        return (style = "incorrect");
+      }
+      if (option === correctOption) {
+        return (style = "correct");
+      }
+    }
+  };
+
+  const getOptions = options.map((el) => {
+    const option = decodeURIComponent(el);
     let styles = getStyles(option);
-  
-    return <button className={`optionButton ${(props.over || props.isNewGame) && "resultOptionButton"}`} 
-    key={option}
+
+    return (
+      <button
+        className={`optionButton ${
+          (props.over || props.isNewGame) && "resultOptionButton"
+        } ${styles}`}
+        key={option}
         disabled={props.over ? true : false}
         onClick={() => {
-          setCurrentAnswer(option)
-          props.handleClick(option, props.questionNumber)}}
-        style={styles} >{option}</button>
-  })
+          setCurrentAnswer(option);
+          props.handleClick(option, props.questionNumber);
+        }}
+        // style={styles}
+      >
+        {option}
+      </button>
+    );
+  });
 
   return (
     <div className="question-container">
       <h3 className="question-title">{props.question}</h3>
-      <div className="question-options">
-        {getOptions}
-      </div>
+      <div className="question-options">{getOptions}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
